2c5d6f91f68a38414306702b044014e436aa8c09
Ay Has <ahashim983@yahoo.com>
2019-04-01 14:13:54 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -43,7 +43,7 @@ def get_first_name_of_season_winner(data, season)
 end
 #puts data.keys
 #puts data.keys[0]
-puts data.keys[0].values
+puts data.keys.values
 def get_contestant_name(data, occupation)
 
   # code here
