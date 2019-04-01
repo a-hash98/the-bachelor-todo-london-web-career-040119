9c5d76a8988dbe03d188ffad1eb7d7f6bfa9a9c2
Ay Has <ahashim983@yahoo.com>
2019-04-01 13:59:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   for i in data[:season][:status]
-    if data[:season][:status] == "Winner"
-      return data[:season][:name]
+    if i == "Winner"
+      return i[:season][:name]
     end
   end
   # code here
